33a62ae10c53275d107cab5d60affdd68903ad1d
Waterburner <Waterburner@users.noreply.github.com>
2021-08-06 11:50:28 -0500

    testing

--- src/components/pages/test.js
+++ src/components/pages/test.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default function() {
     return (
         <div>
-            <h2>testing commit-push thingy</h2>
+            <h2>testing commit-push thingy again</h2>
         </div>
     );
 }
