ce3c5f483b114cbf0a34fdad6b10ef75ea5c427d
Waterburner <Waterburner@users.noreply.github.com>
2021-08-23 22:14:38 -0500

    corrected phone number

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -4,47 +4,47 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import contactPagePicture from "";
 import contactPagePicture from "../../../static/assets/img/contact/20200131contact01.jpg";
 
-export default function() {
-  return (
-    <div className="content-page-wrapper">
-      <div
-        className="left-column"
-        style={{
-          background: "url(" + contactPagePicture + ") no-repeat",
-          backgroundSize: "cover",
-          backgroundPosition: "center"
-        }}
-      />
-      <div className="right-column">
-        <div className="contact-bullet-points">
-          <div className="bullet-point-group">
-            <div className="icon">
-              <FontAwesomeIcon icon="phone" />
+export default function () {
+    return (
+        <div className="content-page-wrapper">
+            <div
+                className="left-column"
+                style={{
+                    background: "url(" + contactPagePicture + ") no-repeat",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                }}
+            />
+            <div className="right-column">
+                <div className="contact-bullet-points">
+                    <div className="bullet-point-group">
+                        <div className="icon">
+                            <FontAwesomeIcon icon="phone" />
+                        </div>
+
+                        <div className="text">409 392 6506</div>
+                    </div>
+
+                    <div className="bullet-point-group">
+                        <div className="icon">
+                            <FontAwesomeIcon icon="envelope" />
+                        </div>
+
+                        <div className="text">oleh.kovelskyi@email.com</div>
+                    </div>
+
+                    <div className="bullet-point-group">
+                        <div className="icon">
+                            <FontAwesomeIcon icon="map-marked-alt" />
+                        </div>
+
+                        <div className="text">
+                            2101 Church street <br />
+                            Galveston, TX
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            <div className="text">409 392 6505</div>
-          </div>
-
-          <div className="bullet-point-group">
-            <div className="icon">
-              <FontAwesomeIcon icon="envelope" />
-            </div>
-
-            <div className="text">oleh.kovelskyi@email.com</div>
-          </div>
-
-          <div className="bullet-point-group">
-            <div className="icon">
-              <FontAwesomeIcon icon="map-marked-alt" />
-            </div>
-
-            <div className="text">
-              2101 Church street <br/>
-              Galveston, TX
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
